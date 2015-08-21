9964a2cf34ea5964643e755f05140ae3c6261566
nide <nenegoro@gmail.com>
2015-08-21 19:22:51 +0300

    Added tests for newline rule

--- tests.js
+++ tests.js
@@ -7,6 +7,43 @@ var
 var
 	eslintTester = new ESLintTester(eslint);
 
+eslintTester.addRuleTest('newline-around-multiline-blocks', {
+	valid: [
+		'someCall(param);\nobject.method(objectParam);\nreturn this;',
+		'tryThisone(param).andChainCall(here);\n\n# This is comment\ncommented.call();',
+		'object.method(objectParam);\nanotherObject(anotherParam);\n\nif(1 > 0)\n\tcall();',
+		'call.method();\n\ndictionary = {\n\tanother_key: V\n}',
+		'dictionary = {\n\tanother_key: V,\n\tbb: value,\n\n\tlargeValue: [\n\t\t1,\n\t\t2,\n\t\t3,\n\t\t4\n\t],\n\n\tzz: 333};'
+	],
+
+	invalid: [
+		{
+			code: 'someCall(param);\nobject.method(objectParam);\n\nreturn this;',
+			errors: [{message: 'There should not be new line between singleline nodes', type: 'BlockStatement'}]
+		},
+
+		{
+			code: 'object.method(objectParam);\nanotherObject(anotherParam);\nif(1 > 0)\n\tcall();',
+			errors: [{message: 'Multiline node should have trailing new line', type: 'BlockStatement'}]
+		},
+
+		{
+			code: 'call.method();\ndictionary = {\n\tanother_key: V\n}',
+			errors: [{message: 'Singleline node prior to multiline should have trailing new line', type: 'BlockStatement'}]
+		},
+
+		{
+			code: '[\n\t2,\n\t{\n\t\tkey: value,\n\t\tanotherKey: anotherValue\n\t}\n]',
+			errors: [{message: 'Singleline node prior to multiline should have trailing new line', type: 'ArrayExpression'}]
+		},
+
+		{
+			code: '{\n\tkey: 2,\n\tanotherKey: {\n\t\tkey: value,\n\t\tanotherKey: anotherValue\n\t}\n}',
+			errors: [{message: 'Singleline node prior to multiline should have trailing new line', type: 'ObjectExpression'}]
+		}
+	]
+});
+
 eslintTester.addRuleTest('no-var-on-same-line', {
 	valid: [
 		'callFoo();\nvar\n\tvariable = 1',
