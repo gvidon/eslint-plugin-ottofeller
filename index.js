e07ccb93803a6bc620cd34dbf078fa7e5a539ab5
Gvidon Malyarov <gvidon@ottofeller.com>
2015-10-22 15:57:16 +0300

    Added missed quote

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ module.exports = {
 	rules: {
 		'newline-around-multiline-blocks': require('./lib/rules/newline-around-multiline-blocks'),
 		'no-var-on-same-line': require('./lib/rules/no-var-on-same-line'),
-		'no-full-name-lambda-params': require('./lib/rules/no-full-name-lambda-params)
+		'no-full-name-lambda-params': require('./lib/rules/no-full-name-lambda-params')
 	}
 };
